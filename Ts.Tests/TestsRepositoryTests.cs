9e6b64d9d74ef10c2bead5e21ec9e7c7011fa0fe
Igor Daub <igordaub@gmail.com>
2024-02-17 18:00:18 +0100

    - get tests reastored

--- Ts.Tests/TestsRepositoryTests.cs
+++ Ts.Tests/TestsRepositoryTests.cs
@@ -9,112 +9,110 @@ using TS.Data;
 using TS.Data.Contracts.DTO;
 using TS.Data.Contracts.Entities;
 using TS.Data.Repositories;
+using TS.Services;
 
 namespace Ts.Tests
 {
     public class TestsRepositoryTests
     {
-        //private readonly TestsRepository _repos;
-        //private readonly Mock<TestsContext> _contextMock;
-        //public TestsRepositoryTests()
-        //{
-        //    _contextMock = new TestsContextMock().CreateMock();
-        //    _repos = new TestsRepository(_contextMock.Object);
-        //}
+        TestsService _service;
+        public TestsRepositoryTests()
+        {
+            Mock<TestsContext> contextMock = new TestsContextMock().CreateMock();
+            TestsContentRepository contentRepos = new TestsContentRepository(contextMock.Object);
+            TestDescriptionsRepository DescriptionsRepos = new TestDescriptionsRepository(contextMock.Object);
+            _service = new TestsService(null,contentRepos, DescriptionsRepos);
+        }
 
-        //// Add depricated version and wrong Id
-        //[Fact]
-        //public async Task GetTestDescriptionById_Deleted_Ok()
-        //{
-        //    TestDescriptions res = await _repos.GetTestDescriptionById(TSConstants.FirstContextDescription.Id);
+        [Fact]
+        public async Task GetTestDescriptionById_Deleted_Ok()
+        {
+            var res = await _service.GetTestDescriptionById(TSConstants.FirstContextDescription.Id);
 
-        //    Assert.True(res.Id == TSConstants.FirstContextDescription.Id && res.ImmutableId == TSConstants.FirstContextDescription.ImmutableId);
-        //}
+            Assert.True(res.Id == TSConstants.FirstContextDescription.Id && res.ImmutableId == TSConstants.FirstContextDescription.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestDescriptionById_Actual_Ok()
-        //{
-        //    TestDescriptions res = await _repos.GetTestDescriptionById(TSConstants.FirstUpdContextDescription.Id);
+        [Fact]
+        public async Task GetTestDescriptionById_Actual_Ok()
+        {
+            TestDescriptions res = await _service.GetTestDescriptionById(TSConstants.FirstUpdContextDescription.Id);
 
-        //    Assert.True(res.Id == TSConstants.FirstUpdContextDescription.Id && res.ImmutableId == TSConstants.FirstUpdContextDescription.ImmutableId);
-        //}
+            Assert.True(res.Id == TSConstants.FirstUpdContextDescription.Id && res.ImmutableId == TSConstants.FirstUpdContextDescription.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestDescriptionById_WrongId_ThrowsException()
-        //{
-        //    await Assert.ThrowsAsync<EntityNotFoundException>(() => _repos.GetTestDescriptionById(int.MaxValue));
-        //}
+        [Fact]
+        public async Task GetTestDescriptionById_WrongId_ThrowsException()
+        {
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => _service.GetTestDescriptionById(int.MaxValue));
+        }
 
-        //[Fact]
-        //public async Task GetAllDescriptions_moreThanPage_Ok()
-        //{
-        //    PaginatedResponse<TestDescriptions> res = await _repos.GetAllDescriptions(TSConstants.FirstUserGuid,
-        //                                                                              new PaginationRequest(1, 10));
-        //    Assert.True(res.AllEntriesCount > 0 && res.Result.Count == res.AllEntriesCount);
-        //    Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
-        //}
+        [Fact]
+        public async Task GetAllDescriptions_moreThanPage_Ok()
+        {
+            PaginatedResponse<TestDescriptions> res = await _service.GetAllDescriptions(new PaginationRequest(1, 10), TSConstants.FirstUserGuid);
+            Assert.True(res.AllEntriesCount > 0 && res.Result.Count == res.AllEntriesCount);
+            Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
+        }
 
-        //[Fact]
-        //public async Task GetAllDescriptions_LessthanPage_Ok()
-        //{
-        //    int pageLength = 2;
-        //    PaginatedResponse<TestDescriptions> res = await _repos.GetAllDescriptions(TSConstants.FirstUserGuid,
-        //                                                                              new PaginationRequest(1, pageLength));
-        //    Assert.True(res.AllEntriesCount > 0
-        //    && res.Result.Count < res.AllEntriesCount
-        //    && res.Result.Count == pageLength);
-        //    Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
-        //}
+        [Fact]
+        public async Task GetAllDescriptions_LessthanPage_Ok()
+        {
+            int pageLength = 2;
+            PaginatedResponse<TestDescriptions> res = await _service.GetAllDescriptions(new PaginationRequest(1, pageLength), TSConstants.FirstUserGuid);
+            Assert.True(res.AllEntriesCount > 0
+            && res.Result.Count < res.AllEntriesCount
+            && res.Result.Count == pageLength);
+            Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
+        }
 
-        //[Fact]
-        //public async Task GetAllDescriptions_SecondAndLastPage_Ok()
-        //{
-        //    int pageLength = 2;
-        //    int pageCount = 2;
-        //    PaginatedResponse<TestDescriptions> res = await _repos.GetAllDescriptions(TSConstants.FirstUserGuid,
-        //                                                                              new PaginationRequest(pageCount, pageLength));
-        //    Assert.True(res.AllEntriesCount > 0
-        //        && res.Result.Count < res.AllEntriesCount
-        //        && res.Result.Count < pageCount * pageLength);
-        //    Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
-        //}
+        [Fact]
+        public async Task GetAllDescriptions_SecondAndLastPage_Ok()
+        {
+            int pageLength = 2;
+            int pageCount = 2;
+            PaginatedResponse<TestDescriptions> res = await _service.GetAllDescriptions(new PaginationRequest(pageCount, pageLength), TSConstants.FirstUserGuid);
+            Assert.True(res.AllEntriesCount > 0
+                && res.Result.Count < res.AllEntriesCount
+                && res.Result.Count < pageCount * pageLength);
+            Assert.True(res.Result.All(r => r.CrreatedBy == TSConstants.FirstUserGuid));
+        }
 
-        //[Fact]
-        //public async Task GetTestContentByDescriptionsId_DeletedDescription_Ok()
-        //{
-        //    TestsContent res = await _repos.GetTestContentByDescriptionsId(TSConstants.FirstContextDescription.Id);
+        [Fact]
+        public async Task GetTestContentByDescriptionsId_DeletedDescription_Ok()
+        {
+            TestsContent res = await _service.GetTestContentByDescriptionsId(TSConstants.FirstContextDescription.Id);
 
-        //    Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
-        //}
+            Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestContentByDescriptionsId_ActualDescription_Ok()
-        //{
-        //    TestsContent res = await _repos.GetTestContentByDescriptionsId(TSConstants.FirstUpdContextDescription.Id);
-        //    Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
-        //}
+        [Fact]
+        public async Task GetTestContentByDescriptionsId_ActualDescription_Ok()
+        {
+            TestsContent res = await _service.GetTestContentByDescriptionsId(TSConstants.FirstUpdContextDescription.Id);
+            Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestContentByDescriptionsImmutableId_DeletedContext_Ok()
-        //{
-        //    TestsContent res = await _repos.GetTestContentByDescriptionsImmutableId(TSConstants.FirstContextDescription.ImmutableId);
+        [Fact]
+        public async Task GetTestContentByDescriptionsImmutableId_DeletedContext_Ok()
+        {
+            TestsContent res = await _service.GetTestContentByDescriptionsImmutableId(TSConstants.FirstContextDescription.ImmutableId);
 
-        //    Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
-        //}
+            Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestContentByDescriptionsImmutableId_ActualContext_Ok()
-        //{
-        //    TestsContent res = await _repos.GetTestContentByDescriptionsImmutableId(TSConstants.FirstUpdContextDescription.ImmutableId);
+        [Fact]
+        public async Task GetTestContentByDescriptionsImmutableId_ActualContext_Ok()
+        {
+            TestsContent res = await _service.GetTestContentByDescriptionsImmutableId(TSConstants.FirstUpdContextDescription.ImmutableId);
 
-        //    Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
-        //}
+            Assert.True(res.Id == TSConstants.UpdSingleOptionContent.Id && res.ImmutableId == TSConstants.UpdSingleOptionContent.ImmutableId);
+        }
 
-        //[Fact]
-        //public async Task GetTestContentByDescriptionsImmutableId_WrongImmutableId_ThrowsException()
-        //{
-        //    await Assert.ThrowsAsync<EntityNotFoundException>(() => _repos.GetTestContentByDescriptionsImmutableId(Guid.NewGuid()));
-        //}
+        [Fact]
+        public async Task GetTestContentByDescriptionsImmutableId_WrongImmutableId_ThrowsException()
+        {
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => _service.GetTestContentByDescriptionsImmutableId(Guid.NewGuid()));
+        }
 
         //[Fact]
         //public async Task CreateNewTest_Ok()
